1092b68f0c19160db08b24a5b22b697bcabe784a
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-02-05 14:03:37 -0600

    Consolidate conversion tools

--- Dockerfile
+++ Dockerfile
@@ -23,7 +23,7 @@ ENTRYPOINT ["/bin/bash"]
 
 LABEL org.opencontainers.image.authors="William Jackson <william@subtlecoolness.com>"
 
-COPY --chown=python:python convert_to_jpg.py /home/python/file-tools/convert_to_jpg.py
+COPY --chown=python:python convert-to.py /home/python/file-tools/convert-to.py
 COPY --chown=python:python count-extensions.py /home/python/file-tools/count-extensions.py
 COPY --chown=python:python find_duplicates.py /home/python/file-tools/find_duplicates.py
 COPY --chown=python:python find_similar.py /home/python/file-tools/find_similar.py
---
+++ convert-to.py
@@ -0,0 +1,64 @@
+import argparse
+import pathlib
+import subprocess
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Convert media files')
+    parser.add_argument('format', choices=('jpg', 'mp3', 'mp4'), help='target format')
+    parser.add_argument('files', nargs='+', help='files to convert')
+    return parser.parse_args()
+
+
+def to_jpg(source_path: pathlib.Path):
+    target_path = source_path.with_suffix('.jpg')
+    cmd = [
+        '/usr/bin/gm', 'convert',
+        str(source_path),
+        str(target_path)
+    ]
+    subprocess.call(cmd)
+
+
+def to_mp3(source_path: pathlib.Path):
+    target_path = source_path.with_suffix('.mp3')
+    cmd = [
+        '/usr/bin/ffmpeg',
+        '-i',
+        str(source_path),
+        str(target_path)
+    ]
+    subprocess.call(cmd)
+
+
+def to_mp4(source_path: pathlib.Path):
+    target_path = source_path.with_suffix('.mp4')
+    cmd = [
+        '/usr/bin/ffmpeg',
+        '-i', str(source_path),
+        '-c:v', 'libx264',
+        '-c:a', 'aac',
+        str(target_path)
+    ]
+    subprocess.call(cmd)
+
+
+actions = {
+    'jpg': to_jpg,
+    'mp3': to_mp3,
+    'mp4': to_mp4,
+}
+
+
+def main():
+    args = parse_args()
+    total = len(args.files)
+    action = actions.get(args.format)
+    for i, filename in enumerate(args.files, start=1):
+        source_path = pathlib.Path(filename).resolve()
+        print(f'Processing {source_path} ({i} of {total})')
+        action(source_path)
+
+
+if __name__ == '__main__':
+    main()
