fce6a3b7bc3e41a5d3ccb250bda1c62c52eced62
William Jackson <565174+williamjacksn@users.noreply.github.com>
2026-01-14 11:05:56 -0600

    Update extensions in remove_live_photos.py

--- remove_live_photos.py
+++ remove_live_photos.py
@@ -4,8 +4,8 @@ import pathlib
 def main():
     directory = pathlib.Path().resolve()
     for item in directory.iterdir():
-        if item.suffix == ".jpg":
-            item.with_suffix(".mov").unlink(missing_ok=True)
+        if item.suffix == ".HEIC":
+            item.with_suffix(".MP4").unlink(missing_ok=True)
 
 
 if __name__ == "__main__":
